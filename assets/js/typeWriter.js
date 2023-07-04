b56adc1c84a169bbb7df2df298d8d6f522b7fee9
Vítor Arimatéa <vitorodev@outlook.com>
2023-07-04 15:16:05 -0300

    fix: Excluindo carousel e organizando typeWriter

--- assets/js/typeWriter.js
+++ assets/js/typeWriter.js
@@ -1,15 +1,12 @@
-const professionTitle = document.querySelector(".profession-title")
-const text = "Desenvolvedor de Software"
-let index = 0
+const professionTitle = document.querySelector(".profession-title");
+const text = "Desenvolvedor de Software";
+let index = 0;
 
 function typeWriter() {
-  if (index < text.length) {
-    professionTitle.innerHTML += text.charAt(index)
-    index++
-    setTimeout(typeWriter, 50)
-  } else {
-    cursorBlink()
-  }
-}
+  (index < text.length);
+  professionTitle.innerHTML += text.charAt(index);
+  index++;
+  setTimeout(typeWriter, 50);
+};
 
 typeWriter();
