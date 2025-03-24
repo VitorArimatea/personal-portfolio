c85e1bf614a911cd7993b562564865fe9dd28df5
VitorArimatea <vitorarimatea@hotmail.com>
2025-03-24 09:55:31 -0300

    feat: add data.ts and merge utils, export to Next.js config

---
+++ data/index.ts
@@ -0,0 +1,121 @@
+export const navItems = [
+  { name: "Início", link: "/home" },
+  { name: "Projetos", link: "/projects" },
+  { name: "Sobre", link: "/about" },
+  { name: "Tecnologias", link: "/stack" },
+  { name: "Contato", link: "/contact" },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Projeto 1",
+    des: "Em desenvolvimento ...",
+    img: "",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/projeto1.com",
+  },
+  {
+    id: 2,
+    title: "Projeto 2",
+    des: "Em desenvolvimento ...",
+    img: "",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/projeto2.com",
+  },
+  {
+    id: 3,
+    title: "Projeto 3",
+    des: "Em desenvolvimento ...",
+    img: "",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/projeto3.com",
+  },
+  {
+    id: 4,
+    title: "Projeto 4",
+    des: "Em desenvolvimento ...",
+    img: "",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/projeto4.com",
+  },
+];
+
+export const aboutItems = [
+  {
+    id: 1,
+    title: "Busco desenvolver e resolver problemas da melhor forma",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Tenho flexibilidade para trabalhar de qualquer lugar",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Tech Stack",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center w-[30vw]",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Entusiasta apaixonado por tecnologia",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Sou um desenvolvedor Full-Stack com 3 anos de experiência",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Vamos começar um projeto juntos?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/github.svg",
+    link: "https://github.com/VitorArimatea",
+    alt: "Ícone do GitHub",
+  },
+  {
+    id: 2,
+    img: "/linkedin.svg",
+    link: "https://www.linkedin.com/in/vitorarimatea/",
+    alt: "Ícone do LinkedIn",
+  },
+];
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  //output: 'export',
+  /*typescript: {
+    ignoreBuildErrors: true,
+  }*/
 };
 
 export default nextConfig;
