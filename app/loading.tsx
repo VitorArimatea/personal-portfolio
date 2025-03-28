503b814b2836f48ed297a15e2327f355394f9d02
VitorArimatea <vitorarimatea@hotmail.com>
2025-03-28 11:45:36 -0300

    feat: create simple loading component

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-[#FEFCFB] dark:bg-[#0A1128]">
+      <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
