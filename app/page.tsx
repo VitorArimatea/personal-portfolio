8bf6ad147e0096a50894875d8f7dee103a90289e
VitorArimatea <vitorarimatea@hotmail.com>
2025-03-31 10:21:30 -0300

    refactor: loading spinner changed and added acessibility aria

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-
 import Loading from "./loading";
+import { useEffect, useState } from "react";
 
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import AnimatedSection from "@/components/AnimatedSection";
 import RecentProjects from "@/components/RecentProjects";
 import { Skills } from "@/components/Skills";
 import { MyTechs } from "@/components/MyTechs";
@@ -16,43 +15,37 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setIsLoading(false), 1000);
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 700);
+
+    return () => clearTimeout(timer);
   }, []);
 
-  if (isLoading) return <Loading />;
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
-    <div className="relative bg-[#FEFCFB] dark:bg-[#0A1128] flex justify-center items-center flex-col overflow-x-hidden">
+    <div
+      className="relative bg-background dark:bg-background flex justify-center items-center flex-col overflow-hidden"
+      aria-label="Página inicial do site"
+    >
       <Navbar />
       <div className="max-w-7xl w-full px-5 sm:px-10">
         <Hero />
 
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, ease: "easeOut" }}
-          viewport={{ once: true }}
-        >
+        <AnimatedSection>
           <RecentProjects />
-        </motion.div>
+        </AnimatedSection>
 
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, ease: "easeOut", delay: 0.2 }}
-          viewport={{ once: true }}
-        >
+        <AnimatedSection delay={0.2}>
           <Skills />
-        </motion.div>
+        </AnimatedSection>
 
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, ease: "easeOut", delay: 0.4 }}
-          viewport={{ once: true }}
-        >
+        <AnimatedSection delay={0.4}>
           <MyTechs />
-        </motion.div>
+        </AnimatedSection>
 
         <Footer />
       </div>
