e3feedd61b9c1b2aa2392b23e92d36a0fac4b91a
VitorArimatea <vitorarimatea@hotmail.com>
2025-03-31 10:27:39 -0300

    refactor: loading spinner changed

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,15 @@
-const Loading = () => {
+import RingLoader from "react-spinners/RingLoader";
+
+const loading = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-[#FEFCFB] dark:bg-[#0A1128]">
-      <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    <div className="h-screen flex justify-center items-center">
+      <RingLoader
+        color={`#8BC0FF`}
+        size={150}
+        aria-label="Tela de carregamento"
+        data-testid="loader"
+      />
     </div>
   );
 };
-
-export default Loading;
+export default loading;
