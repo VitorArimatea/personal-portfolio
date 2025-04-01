4e632271fb4e0737f86bccc1973bed00ea2892ca
VitorArimatea <vitorarimatea@hotmail.com>
2025-04-01 10:50:52 -0300

    refactor: updated conversion phrase and add real e-mail contact

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,21 +2,20 @@ import Button from "./ui/AnimatedButton";
 import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "@/data";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="pt-20 pb-10 px-5 bg-[#111]" id="contact">
+    <footer className="pt-20 pb-10" id="contact">
       <div className="flex flex-col items-center">
-        <h1 className="heading lg:max-w-[45vw] text-[#f1f1f1]">
-          Pronto para levar{" "}
-          <span className="text-blue-400">sua presença digital</span> ao próximo
-          nível?
+        <h1 className="heading lg:max-w-[45vw] dark:text-[#f1f1f1]">
+          Pronto para colaborar no seu{" "}
+          <span className="text-blue-400">próximo projeto.</span>
         </h1>
-        <p className="text-[#e2e2e2] mt-5 md:mt-10 text-center">
-          Entre em contato comigo hoje mesmo e vamos discutir como posso ajudar
-          você a atingir seus objetivos.
+        <p className="dark:text-[#e2e2e2] mt-5 md:mt-10 text-center">
+          Entre em contato comigo hoje mesmo.
         </p>
-        <a href="mailto:contact@vitorarimatea.com">
+        <a href="mailto:contato@vitorarimatea.com.br">
           <Button
             title="Entrar em contato"
             icon={<FaLocationArrow />}
@@ -26,7 +25,7 @@ const Footer = () => {
       </div>
 
       <div className="flex mt-10 md:mt-15 flex-col-reverse gap-5 md:flex-row justify-between items-center">
-        <p className="text-[13px] md:text-base font-light md:font-normal text-[#fff]">
+        <p className="text-[13px] md:text-base font-light md:font-normal dark:text-[#fff]">
           Copyright © 2025 Vítor Arimatéa - Todos os direitos reservados.
         </p>
 
@@ -36,9 +35,14 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center"
             >
-              <a href={info.link} target="_blank" rel="noopener noreferrer" className="">
+              <Link
+                href={info.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="rounded-md bg-[#0a1128] dark:bg-transparent p-2"
+              >
                 <img src={info.img} alt={info.alt} width={30} height={30} />
-              </a>
+              </Link>
             </div>
           ))}
         </div>
