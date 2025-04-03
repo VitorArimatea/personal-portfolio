9cc2c5fab101b1b22d341e1daa2b9ff8973d9670
VitorArimatea <vitorarimatea@hotmail.com>
2025-04-03 11:55:18 -0300

    refactor: improve RecentProjects responsiveness and remove unnecessary code.Update PinContainer with new bottom link effect color

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,78 +1,71 @@
-"use client";
+import { recentProjects } from "@/data";
 
-import { FaLocationArrow } from "react-icons/fa6";
-
-import { projects } from "@/data";
+import Link from "next/link";
 import { PinContainer } from "./ui/PinContainer";
-
+import Image from "next/image";
+import { FaLocationArrow } from "react-icons/fa6";
+import Title from "./ui/Title";
 
 const RecentProjects = () => {
   return (
-    <section className="py-20" id="projects">
-      
-        <h1 className="heading">
-          Uma pequena seleção dos{" "}
-          <span className="text-blue-400">Meus Projetos</span>
-        </h1>
-      
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 md:gap-30 mt-10">
-        {projects.map((item) => (
+    <section className="py-10" id="recentProjects">
+      <Title highlight="Projetos">Meus melhores</Title>
+
+      <div className="flex flex-wrap justify-center py-10 md:gap-25 lg:gap-30">
+        {recentProjects.map((project) => (
           <div
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-            key={item.id}
+            className="h-[30rem] flex items-center justify-center w-96"
+            key={project.id}
           >
-            <PinContainer title={item.link} href={item.link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <div
-                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                  style={{ backgroundColor: "#13162D" }}
-                >
-                  <img src="/bg.png" alt="bgimg" />
-                </div>
-                <img
-                  src={item.img}
-                  alt="cover"
-                  className="z-10 absolute bottom-0"
-                />
-              </div>
-
-              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                {item.title}
-              </h1>
-
-              <p
-                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                style={{
-                  color: "#BEC1DD",
-                  margin: "1vh 0",
-                }}
-              >
-                {item.des}
-              </p>
-
-              <div className="flex items-center justify-between mt-7 mb-3">
-                <div className="flex items-center">
-                  {item.iconLists.map((icon, index) => (
-                    <div
-                      key={index}
-                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                      style={{
-                        transform: `translateX(-${5 * index + 2}px)`,
-                      }}
-                    >
-                      <img src={icon} alt="icon5" className="p-2" />
-                    </div>
-                  ))}
+            <Link href={project.link}>
+              <PinContainer title={project.link}>
+                <div className="relative flex items-center justify-center w-75 sm:w-100 overflow-hidden h-[23vh] sm:h-[30vh] mb-10">
+                  <Image
+                    src={project.img}
+                    alt="Pré visualização do projeto"
+                    height={400}
+                    width={400}
+                  />
                 </div>
 
-                <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Visualizar site
-                  </p>
-                  <FaLocationArrow className="ms-3" color="#CBACF9" />
+                <h1 className="font-bold md:text-xl lg:text-2xl">
+                  {project.title}
+                </h1>
+
+                <p className="text-sm lg:text-xl font-light md:font-normal text-[#0a1128] dark:text-blue-50 my-1">
+                  {project.des}
+                </p>
+
+                <div className="flex items-center justify-between mt-7 mb-3">
+                  <div className="flex items-center">
+                    {project.iconLists.map((icon, index) => (
+                      <div
+                        key={index}
+                        className="border border-white/[.2] rounded-full bg-[#111] w-8 h-8 lg:w-10 lg:h-10 flex justify-center items-center"
+                        style={{
+                          transform: `translateX(-${5 * index + 2}px)`,
+                        }}
+                      >
+                        <Image
+                          src={icon}
+                          alt="Ícone da tecnologia"
+                          className="p-2"
+                          height={50}
+                          width={50}
+                        />
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="flex justify-center items-center">
+                    <p className="flex text-sm md:text-xs lg:text-xl">
+                      Visualizar site
+                    </p>
+                    <FaLocationArrow className="ms-3" color="#85BDFF" />
+                  </div>
                 </div>
-              </div>
-            </PinContainer>
+              </PinContainer>
+            </Link>
           </div>
         ))}
       </div>
