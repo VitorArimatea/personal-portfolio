3bf948d1e662c6fd1910fcc11d1f4bc0de232d25
VitorArimatea <vitorarimatea@hotmail.com>
2025-05-05 14:35:55 -0300

    chore: create technologies data array

--- data/index.ts
+++ data/index.ts
@@ -1,42 +1,68 @@
 export const navItems = [
-  { name: "Página inicial", link: "/" },
+  { name: "Início", link: "/" },
   { name: "Projetos", link: "/projects" },
   { name: "Sobre", link: "/about" },
-  { name: "Tecnologias", link: "/skills" },
+  { name: "Serviços", link: "/services" },
 ];
 
-export const projects = [
+export const recentProjects = [
   {
     id: 1,
     title: "imSafeBank",
     des: "Uma landing page de um banco moderno conceitual",
-    img: "p1.svg",
-    iconLists: ["/ts.svg", "/react.svg", "/tail.svg"],
+    img: "/p1.svg",
+    iconLists: ["/ts.svg", "/react.svg", "/tail.svg", "/vite.svg"],
     link: "https://im-safe-bank.vercel.app/",
   },
   {
     id: 2,
     title: "Weather Now",
-    des: "App que possibilita a visualização do clima em tempo real",
-    img: "p2.svg",
-    iconLists: ["/ts.svg", "/react.svg", "/tail.svg"],
+    des: "Website que possibilita a visualização do clima em tempo real",
+    img: "/p2.svg",
+    iconLists: ["/js.svg", "/react.svg", "/tail.svg"],
     link: "https://github.com/VitorArimatea/weather-now",
   },
   {
     id: 3,
-    title: "Apenas filmes",
-    des: "Um website para ver informações sobre filmes",
-    img: "p3.svg",
-    iconLists: ["/ts.svg", "/react.svg", "/tail.svg"],
-    link: "https://im-safe-bank.vercel.app/",
+    title: "Notes",
+    des: "Um website para criar notas utilizando tanto texto quanto voz",
+    img: "/p3.svg",
+    iconLists: ["/ts.svg", "/react.svg", "/tail.svg", "/vite.svg"],
+    link: "https://notes-taupe-theta.vercel.app/",
   },
   {
     id: 4,
-    title: "Pokedex",
-    des: "Uma pokedex, que lista todos os pokemons",
-    img: "p4.svg",
-    iconLists: ["/ts.svg", "/react.svg", "/tail.svg"],
-    link: "https://im-safe-bank.vercel.app/",
+    title: "Bustley",
+    des: "Uma rede social completamente funcional, inspirada no antigo twitter",
+    img: "/p4.svg",
+    iconLists: [
+      "/ts.svg",
+      "/react.svg",
+      "/next.svg",
+      "/tail.svg",
+      "/prisma.svg",
+      "/mongodb.svg",
+    ],
+    link: "https://github.com/VitorArimatea/bustley-social-media",
+  },
+];
+
+export const moreProjects = [
+  {
+    id: 5,
+    title: "Zeus Advogados",
+    des: "Um portal completo, conceitual para advogados",
+    img: "/p5.svg",
+    iconLists: ["/wordpress.svg"],
+    link: "https://indigo-buffalo-645840.hostingersite.com/",
+  },
+  {
+    id: 6,
+    title: "Blog Games",
+    des: "Um blog sobre games criado em angular",
+    img: "/p4.svg",
+    iconLists: ["/ts.svg"],
+    link: "https://github.com/VitorArimatea/bustley-social-media",
   },
 ];
 
@@ -104,17 +130,71 @@ export const aboutItems = [
   },
 ];
 
+const technologies = [
+  {
+    name: "TypeScript",
+    description:
+      "Uma linguagem de programação baseada em JavaScript que adiciona tipagem estática opcional.",
+    icon: "ts.svg",
+  },
+  {
+    name: "React",
+    description:
+      "Uma biblioteca JavaScript para criar interfaces de usuário dinâmicas e reutilizáveis.",
+    icon: "react.svg",
+  },
+  {
+    name: "Next.js",
+    description:
+      "Framework para React que permite renderização do lado do servidor e geração de sites estáticos.",
+    icon: "next.svg",
+  },
+  {
+    name: "Framer motion",
+    description:
+      "biblioteca de animações para React, que permite criar animações complexas e interações dinâmicas com facilidade e desempenho otimizado.",
+    icon: "fm.svg",
+  },
+  {
+    name: "Tailwind CSS",
+    description:
+      "Um framework CSS utilitário que facilita a criação de designs rápidos e responsivos.",
+    icon: "tail.svg",
+  },
+];
+
+const moreTechnologies = [
+  {
+    name: "MongoDB",
+    description:
+      "Um SGBD NoSQL, armazena os dados em documentos no formato JSON, permitindo mais flexibilidade na organização das informações.",
+    icon: "/mongodb.svg",
+  },
+  {
+    name: "Prisma",
+    description:
+      "Ele simplifica a escrita de queries, tornando o código mais limpo, seguro e fácil de manter.",
+    icon: "/prisma-black.svg",
+  },
+  {
+    name: "MySQL",
+    description:
+      "Um dos bancos de dados SQL mais populares do mundo. Ele organiza os dados em tabelas, onde cada coluna tem um tipo de dado definido.",
+    icon: "/mysql.svg",
+  },
+];
+
 export const socialMedia = [
   {
     id: 1,
     img: "/github.svg",
     link: "https://github.com/VitorArimatea",
-    alt: "Ícone do GitHub",
+    alt: "Meu GitHub",
   },
   {
     id: 2,
     img: "/linkedin.svg",
     link: "https://www.linkedin.com/in/vitorarimatea/",
-    alt: "Ícone do LinkedIn",
+    alt: "Meu LinkedIn",
   },
 ];
