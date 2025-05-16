4f93114cb419d7543d9f9b79160fd7b42b22d759
VitorArimatea <vitorarimatea@hotmail.com>
2025-05-16 11:17:05 -0300

    fix: loader color issue in light theme

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,24 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
 import RingLoader from "react-spinners/RingLoader";
 
 const loading = () => {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <div className="h-screen flex justify-center items-center">
       <RingLoader
-        color={`#8BC0FF`}
+        color={theme === "dark" ? "#8BC0FF" : "#1B4BC6"}
         size={150}
         aria-label="Tela de carregamento"
         data-testid="loader"
