7b69d64fe196905f0041041ef32d0f2d79069223
VitorArimatea <vitorarimatea@hotmail.com>
2025-07-29 10:39:28 -0300

    refactor: add all projects to page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,35 +1,25 @@
-"use client";
-
-import Navbar from "@/components/Navbar";
 import RecentProjects from "@/components/RecentProjects";
-import { Spotlight } from "@/components/ui/Spotlight";
-import { SpotlightNew } from "@/components/ui/SpotlightNew";
-import { motion } from "framer-motion";
 
-const Projects = () => {
+import Spotlight from "@/components/ui/Spotlight";
+import SpotlightNew from "@/components/ui/SpotlightNew";
+import PageTransition from "@/components/animations/PageTransition";
+import Title from "@/components/common/Title";
+
+const AllProjects = () => {
   return (
-    <motion.main
-      initial={{ opacity: 0, y: -30 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8, ease: "easeOut" }}
-      className="bg-[#FEFCFB] dark:bg-[#0A1128] flex justify-center items-center flex-col overflow-x-hidden"
-    >
-      <Navbar />
-      <div className="relative overflow-hidden px-5 sm:px-10">
-        <div className="invisible dark:visible">
-          <Spotlight />
-          <SpotlightNew />
-        </div>
-        <motion.div
-          initial={{ opacity: 0, y: -30 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 0.3 }}
-        >
-          <RecentProjects />
-        </motion.div>
+    <div className="overflow-hidden">
+      <div className="hidden dark:flex">
+        <Spotlight className="top-0 h-[50vh] blur-2xl" />
+        <SpotlightNew />
       </div>
-    </motion.main>
+      <PageTransition>
+        <Title highlight="Projetos" className="mt-20">
+          Todos Meus
+        </Title>
+        <RecentProjects showAll={true} />
+      </PageTransition>
+    </div>
   );
 };
 
-export default Projects;
+export default AllProjects;
