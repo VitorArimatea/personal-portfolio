e1af26aea714e4aee8f6a4c7afb5a144fc02333c
VitorArimatea <vitorarimatea@hotmail.com>
2025-07-29 10:54:21 -0300

    add ScrollAnimated component

--- components/animations/heroVariants.ts
+++ components/animations/heroVariants.ts
@@ -1,4 +1,6 @@
-export const containerVariants = {
+import { Variants } from "framer-motion";
+
+export const containerVariants: Variants = {
   hidden: { opacity: 0 },
   visible: {
     opacity: 1,
@@ -6,7 +8,7 @@ export const containerVariants = {
   },
 };
 
-export const itemVariants = {
+export const itemVariants: Variants = {
   hidden: { opacity: 0, y: -50 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } },
 };
