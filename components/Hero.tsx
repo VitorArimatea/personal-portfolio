f38f8bb3437eefc991431adc572af2dc3b70fe06
VitorArimatea <vitorarimatea@hotmail.com>
2025-07-30 17:03:58 -0300

    refactor: Hero Section updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,6 +6,7 @@ import { containerVariants, itemVariants } from "./animations/heroVariants";
 
 import Spotlight from "./ui/Spotlight";
 import SpotlightNew from "./ui/SpotlightNew";
+import Title from "./common/Title";
 import TextGenerateEffect from "./ui/TextGenerateEffect";
 import Button from "./ui/AnimatedButton";
 
@@ -15,27 +16,22 @@ const Hero = () => {
       initial="hidden"
       animate="visible"
       variants={containerVariants}
-      className="h-[100dvh] flex flex-col gap-4 md:gap-6 justify-center items-center mt-8"
+      className="relative h-[100dvh] flex flex-col gap-4 md:gap-6 justify-center items-center mt-11 dark:mt-8"
     >
-      <div className="invisible dark:sm:visible" aria-hidden="true">
+      <div className="hidden dark:sm:block" aria-hidden="true">
         <SpotlightNew />
         <Spotlight className="absolute inset-x-1/2 top-0 h-[50vh] w-[50vw] opacity-50 blur-2xl -translate-x-1/2" />
       </div>
 
-      <div className="md:invisible">
-        <div className="absolute inset-x-1/2 top-0 h-[30vh] w-[50vw] opacity-40 -translate-x-1/2 white__gradient" />
-      </div>
-
-      <motion.h1
-        variants={itemVariants}
-        className="text-[35px] sm:text-5xl font-bold text-center bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 from-neutral-600 
-        to-neutral-900 bg-clip-text text-transparent max-sm:flex max-sm:flex-col  "
-      >
-        Vítor Arimatéa{" "}
-        <span className="bg-clip-text text-transparent bg-gradient-to-b dark:from-blue-200 dark:to-blue-400 from-[#496dd7] to-[#617fda]">
-          Software Developer
-        </span>
-      </motion.h1>
+      <motion.div variants={itemVariants}>
+        <Title
+          highlight="Desenvolvedor Full-Stack"
+          as="h1"
+          className="text-[35px] sm:text-5xl font-bold text-center"
+        >
+          Vítor Arimatéa
+        </Title>
+      </motion.div>
 
       <motion.div variants={itemVariants}>
         <TextGenerateEffect
@@ -45,12 +41,12 @@ const Hero = () => {
       </motion.div>
 
       <motion.div variants={itemVariants}>
-        <Link href="#recentProjects">
+        <Link href="#projetos-recentes">
           <Button
-            title="Conheça meu trabalho"
+            title="Meus Últimos Projetos"
             icon={<FaLocationArrow />}
             position="right"
-            aria-label="Conheça meu trabalho"
+            aria-label="Meus Últimos Projetos"
           />
         </Link>
       </motion.div>
