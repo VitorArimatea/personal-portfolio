2f51da52bc138f274e9530e99db6c438421c679b
VitorArimatea <vitorarimatea@hotmail.com>
2025-09-04 17:39:57 -0300

    feat: adapt AnimatedButton with new HoverBorderGradient

--- components/ui/AnimatedButton.tsx
+++ components/ui/AnimatedButton.tsx
@@ -1,23 +1,39 @@
-const Button = ({
-    title, icon, position, handleClick, otherClasses
-}: {
-    title: string; 
-    icon: React.ReactNode, 
-    position: string; 
-    handleClick?: () => void;
-    otherClasses?: string 
+"use client";
+
+import React from "react";
+import { HoverBorderGradient } from "../ui/hover-border-gradient";
+
+interface ButtonProps {
+  title: string;
+  icon?: React.ReactNode;
+  position?: "left" | "right";
+  handleClick?: () => void;
+  otherClasses?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  title,
+  icon,
+  position = "left",
+  handleClick,
+  otherClasses = "",
 }) => {
   return (
     <div>
-        <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 mt-10" onClick={handleClick}>
-            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#8c9092_0%,#4e4e4e_50%,#abafb2_100%)]" />
-            <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-[#0b1124] dark:bg-[#0A1128] px-6 text-sm font-medium text-white backdrop-blur-3xl ${otherClasses} gap-2.5`}>
-                {position == 'left' && icon}
-                {title}
-                {position == 'right' && icon}
-            </span>
-        </button>
-  </div>
-  )
-}
-export default Button
+      <HoverBorderGradient
+        as="button"
+        onClick={handleClick}
+        containerClassName="relative inline-flex h-12.5 overflow-hidden rounded-lg p-[1px] md:w-60 mt-10"
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-[#0b1124] dark:bg-[#0A1128] px-6 text-sm font-medium text-white backdrop-blur-3xl gap-2.5 ${otherClasses}`}
+      >
+        {position === "left" && icon}
+
+        <span className="relative z-10">{title}</span>
+
+        {position === "right" && icon}
+      </HoverBorderGradient>
+    </div>
+  );
+};
+
+export default Button;
